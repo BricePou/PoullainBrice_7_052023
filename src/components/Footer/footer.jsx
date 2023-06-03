10284efa367eba9902094cf4a18425d86ea5a966
BricePou <brice.poullain@sfr.fr>
2023-06-03 08:41:48 +0200

    mentorat

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+function Footer() {
+    return (
+        <nav>
+            <Link to=""></Link>
+            <Link to=""></Link>
+        </nav>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+function Header() {
+    return (
+        <nav>
+            <Link to=""></Link>
+            <Link to=""></Link>
+        </nav>
+    )
+}
+
+export default Header
